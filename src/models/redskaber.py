5c354a0b7c543e4c0cd19fe280d0ced4f6a5e843
Christian <s194255@student.dtu.dk>
2024-02-20 13:55:30 +0100

    Update redskaber.py

--- src/models/redskaber.py
+++ src/models/redskaber.py
@@ -21,7 +21,7 @@ class Maskemager(L.LightningModule):
         k = int(maskeringsandel*n_knuder)
         udvalgte_knuder = randperm[:k]
         edge_index2, _, kantmaske = subgraph(udvalgte_knuder, edge_index, return_edge_mask=True)
-        idxs = torch.arange(n_knuder)
+        idxs = torch.arange(n_knuder, device=self.device)
         knudemaske = torch.isin(idxs, edge_index2)
         return {'knuder': knudemaske, 'kanter': kantmaske}
 
