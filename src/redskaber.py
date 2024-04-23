f8eecd067c0f341e3f221a5d3fd88a88d4baed8a
Christian <s194255@student.dtu.dk>
2024-04-23 14:56:53 +0200

    Update redskaber.py

--- src/redskaber.py
+++ src/redskaber.py
@@ -46,7 +46,7 @@ def get_trainer(epoker, logger=None):
         learning_rate_monitor()
     ]
     trainer = L.Trainer(max_epochs=epoker,
-                        log_every_n_steps=1,
+                        log_every_n_steps=50,
                         callbacks=callbacks,
                         precision='16-mixed',
                         logger=logger,
