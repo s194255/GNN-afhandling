83b3e2541dbd592e150ab85971a65b02a9d6c71b
Christian <s194255@student.dtu.dk>
2024-04-26 16:28:22 +0200

    ændringer

--- src/eksp2.py
+++ src/eksp2.py
@@ -118,7 +118,7 @@ class Eksp2:
                             log_every_n_steps=1,
                             callbacks=callbacks,
                             logger=logger,
-                            check_val_every_n_epoch=10,
+                            check_val_every_n_epoch=40,
                             )
         return trainer
     def fortræn(self):
