f04504ab92367af05e6cfb8692a9f970e1bacadf
Christian <s194255@student.dtu.dk>
2024-05-27 16:48:08 +0200

    Update eksp2.py

--- src/eksp2.py
+++ src/eksp2.py
@@ -91,7 +91,6 @@ class Eksp2:
                             check_val_every_n_epoch=config_curr['check_val_every_n_epoch'],
                             gradient_clip_val=config_curr['gradient_clipping'],
                             inference_mode=False,
-                            detect_anomaly=True
                             )
         return trainer
 
