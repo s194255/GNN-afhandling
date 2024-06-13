0a6a3aa18be21a2179e4c4366ad95e55dd5de62e
Christian <s194255@student.dtu.dk>
2024-06-13 21:55:16 +0200

    Update fortræningsfigurer.py

--- src/visualization/fortræningsfigurer.py
+++ src/visualization/fortræningsfigurer.py
@@ -3,11 +3,11 @@ import os
 import matplotlib.pyplot as plt
 import wandb
 import src.visualization.farver as far
-# import pandas as pd
-# from matplotlib.ticker import ScalarFormatter
+import pandas as pd
+from matplotlib.ticker import ScalarFormatter
 # from tqdm import tqdm
 # from src.visualization import viz0
-# import numpy as np
+import numpy as np
 import pickle
 # import random
 
@@ -47,7 +47,19 @@ STJERNER = {
 
 CACHE = "reports/cache/fortræning"
 # COLS = ['lr-AdamW', 'train_loss', 'val_loss']
-COLS = ['train_loss', 'val_loss']
+COLS = ['train_loss', 'val_loss', 'train_lokalt_loss', 'train_globalt_loss']
+FORTRÆ_COLS  = {
+    '3D-EMGP-lokalt': ['train_lokalt_loss', 'val_loss'],
+    '3D-EMGP-globalt': ['train_globalt_loss', 'val_loss'],
+    '3D-EMGP-begge': ['train_lokalt_loss', 'train_globalt_loss', 'train_loss', 'val_loss'],
+    'SelvvejledtQM9': ['train_loss', 'val_loss']
+}
+COL_TITEL = {
+    'train_loss': 'Træningstab (samlet)',
+    'val_loss': 'Valideringstab',
+    'train_lokalt_loss': 'Lokalt tab',
+    'train_globalt_loss': 'Globalt tab',
+}
 X_COL = 'trainer/global_step'
 
 def get_group_dfs(runid):
@@ -78,44 +90,75 @@ def get_group_dfs(runid):
         return dfs
 
 
+def make_table(data: dict):
+    new_df = {col: [] for col in COLS}
+    new_df = {**{'fortræningsudgave': []}, **new_df}
+    new_df = pd.DataFrame(data=new_df)
+    for fortræ, df_dict in data.items():
+        new_df_linje = {'fortræningsudgave': [fortræ]}
+        for col in COLS:
+            if fortræ == 'SelvvejledtQM9' and col in ['train_lokalt_loss', 'train_globalt_loss']:
+                continue
+            df = df_dict[col]
+            df[col] = df[col].apply(pd.to_numeric, errors='coerce')
+            df = df.dropna(how='any')
+            try:
+                new_df_linje[col] = [df[col].min()]
+            except TypeError:
+                a = 2
+        new_df = pd.concat([new_df, pd.DataFrame(data=new_df_linje)], ignore_index=True)
+
 
 def plot(data: dict):
-    fig, axs = plt.subplots(4, 2, figsize=(20, 15))  # Øget figurstørrelse for bedre plads
-    titles = ['Træningstab', 'Valideringstab']  # Tilføjet titler for subplots
+    # fig, axs = plt.subplots(4, len(COLS), figsize=(40, 30))  # Øget figurstørrelse for bedre plads
+    # titles = ['Træningstab', 'Valideringstab']  # Tilføjet titler for subplots
 
     for i, (fortræ, df_dict) in enumerate(data.items()):
-        print(i)
         farve = farveopslag.get(fortræ, 'blue')  # Standardfarve hvis fortræ ikke findes i farveopslag
-        for j, col in enumerate(COLS):
-            ax = axs[i, j]
+        cols = FORTRÆ_COLS[fortræ]
+        fig, axs = plt.subplots(1, len(cols), figsize=(40, 10))
+        for j, col in enumerate(cols):
+            if fortræ == 'SelvvejledtQM9' and col in ['train_lokalt_loss', 'train_globalt_loss']:
+                continue
+            ax = axs[j]
             df = df_dict[col]
             e_p_s = df_dict['epoch']['epoch'].max() / df[X_COL].max()
             df = df[[X_COL, col]].dropna(how='any')
             label = LABELLER[fortræ]
             ax.plot(df[X_COL]*e_p_s, df[col], color=farve, label=label)
-            ax.set_ylabel('MAE', fontsize=30)
-            ax.tick_params(axis='both', which='major', labelsize=25)
-            ax.tick_params(axis='both', which='minor', labelsize=20)
+            if j == 0:
+                ax.set_ylabel('MAE', fontsize=50)
+            ax.tick_params(axis='both', which='major', labelsize=35)
+            ax.tick_params(axis='both', which='minor', labelsize=30)
             ax.grid(True)  # Tilføj grid for bedre læsbarhed
             ax.set_yscale('log')
+            ax.set_xscale('log')
+            # ax.xaxis.set_minor_formatter(ScalarFormatter())
+            # ax.xaxis.set_major_formatter(ScalarFormatter())
+
+            max_epoch = df[X_COL].max() * e_p_s
+            xticks = np.logspace(0, np.log10(max_epoch), num=5)  # Adjust the number of ticks with 'num'
+            rounded_xticks = np.round(xticks).astype(int)
+            ax.set_xticks(rounded_xticks)
+            ax.set_xticklabels(rounded_xticks)
+
+
+
+            # ax.set_xticklabels(np.linspace(0, 50, 10))
             if j == 0:
-                ax.legend(fontsize=30)
+                ax.legend(fontsize=50)
 
-            if i == 0:
-                ax.set_title(titles[j], fontsize=30)  # Tilføjelse af titler til subplots
+            titel = COL_TITEL[col]
+            ax.set_title(titel, fontsize=50)  # Tilføjelse af titler til subplots
+            ax.set_xlabel('Epoke', fontsize=50)
 
-            if i == len(data.items())-1:
-                ax.set_xlabel('Epoke', fontsize=30)
-            # else:
-            #     ax.set_xticklabels([])
-
-    plt.tight_layout()
-    # plt.subplots_adjust(top=0.92)
-    # fig.suptitle('Fortræningernes træningsmetrikker', fontsize=45)  # Tilføj hovedtitel
-    plt.savefig(os.path.join(kørsel_path, f"{FIGNAVN}.jpg"))
-    plt.savefig(os.path.join(kørsel_path, f"{FIGNAVN}.pdf"))
-    plt.legend()
-    plt.close()
+        plt.tight_layout()
+        # plt.subplots_adjust(top=0.92)
+        # fig.suptitle('Fortræningernes træningsmetrikker', fontsize=45)  # Tilføj hovedtitel
+        plt.savefig(os.path.join(kørsel_path, f"{fortræ}.jpg"))
+        plt.savefig(os.path.join(kørsel_path, f"{fortræ}.pdf"))
+        plt.legend()
+        plt.close()
 
 
 
@@ -133,6 +176,7 @@ for fortræ, runid in STJERNER.items():
     group_dfs = get_group_dfs(runid)
     data[fortræ] = group_dfs
 plot(data)
+# make_table(data)
 
 
 # dfs = {}
