d4a49f28f3b912eed45c11e1fe454dfdf9d47fad
Christian <s194255@student.dtu.dk>
2024-06-19 17:38:10 +0200

    ændringer

--- src/fortræn.py
+++ src/fortræn.py
@@ -5,7 +5,6 @@ import os
 import shutil
 import torch
 
-torch.set_float32_matmul_precision('medium')
 
 def parserargs():
     parser = argparse.ArgumentParser(description='Beskrivelse af dit script')
--- src/redskaber.py
+++ src/redskaber.py
@@ -61,7 +61,6 @@ def get_trainer(epoker, logger=None, log_every_n_steps=None):
     trainer = L.Trainer(max_epochs=epoker,
                         log_every_n_steps=log_every_n_steps,
                         callbacks=callbacks,
-                        precision='16-mixed',
                         logger=logger,
                         )
     return trainer
