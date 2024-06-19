c973c8f49abd76b15c30670b3317431aeb314b9e
Christian <s194255@student.dtu.dk>
2024-06-19 21:29:28 +0200

    Update fortræn.py

--- src/fortræn.py
+++ src/fortræn.py
@@ -5,6 +5,7 @@ import os
 import shutil
 import torch
 
+torch.set_float32_matmul_precision('medium')
 
 def parserargs():
     parser = argparse.ArgumentParser(description='Beskrivelse af dit script')
