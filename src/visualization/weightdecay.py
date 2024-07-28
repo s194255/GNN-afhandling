5edfab7ff93e69fc31f1a87173f6d77b4de0a264
Christian <s194255@student.dtu.dk>
2024-07-28 18:21:24 +0200

    Update weightdecay.py

--- src/visualization/weightdecay.py
+++ src/visualization/weightdecay.py
@@ -66,6 +66,22 @@ def nedtaktLilleVsStort():
         print(f"n = {n}")
         print("\n")
 
+def penergi():
+    group_df = viz0.get_group_df('penergi_1', remove_nan=False)
+    fortræer = group_df['fortræningsudgave'].unique()
+    print(fortræer)
+    col = 'test_loss_mean'
+    for fortræ in fortræer:
+        idxs = group_df['fortræningsudgave'] == fortræ
+        mean = group_df[idxs][col].mean()
+        std = group_df[idxs][col].std()
+        n = len(group_df[idxs][col])
+        print(f"fortræ = {fortræ}")
+        print(f"mean = {mean}")
+        print(f"std = {std}")
+        print(f"std/mean = {std / mean}")
+        print(f"n = {n}")
+        print("\n")
 
 def regVsClass():
     runid_to_loss = {
@@ -115,49 +131,5 @@ def weight_decay2():
         print(f"n = {n}")
         print("\n")
 
-
-# kørselid = [0, 1, 2, 3, 4, 5, 6, 7]
-# groups = [f'weightdecay_{k}' for k in kørselid]
-
-# create_cache = False
-#
-# # prefix = 'støjniveau'
-# # kørselid = [0, 1, 2]
-# prefix = 'weightdecay'
-# kørselid = [0, 1, 2, 3, 4, 5, 6, 7]
-#
-#
-# groups = [f'{prefix}_{k}' for k in kørselid]
-# group_dfs = []
-# for k, group in zip(kørselid, groups):
-#     group_df = viz0.get_group_df(group)
-#     weightdecay = group_df['weight_decay'].unique()
-#     print(k)
-#     print(np.log10(weightdecay))
-#     mean = group_df['test_loss_mean'].mean()
-#     std = group_df['test_loss_mean'].std()
-#     print(f"mean = {mean}")
-#     print(f"std = {std}")
-#     print("\n")
-#
-#     group_dfs.append(group_df)
-
-# regVsClass()
-klogtVsDumt()
-# weight_decay2()
-# chance_for_forbedring()
-
-# nedtaktLilleVsStort()
-# kaos()
-
-# group_df = viz0.get_group_df('eksp2-md17_2', remove_nan=False)
-# viz0.sanity_check_group_df(group_df)
-# if create_cache:
-#     df = pd.concat(group_dfs)
-#     cache_folder = os.path.join("reports", "manuelt_arkiv")
-#     os.makedirs(cache_folder, exist_ok=True)
-#     path = os.path.join(cache_folder, f"{prefix}.pickle")
-#     cache = {'df': df}
-#     with open(path, 'wb') as f:
-#         pickle.dump(cache, f)
+penergi()
 
